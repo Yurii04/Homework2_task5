c508980e4b81681d332c4540396bf009ac5be272
Yurii <yuriisupkaiev@icloud.com>
2021-08-30 21:47:36 +0300

    task4

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function factorialize(num) {
-  let result = 1;
-  for (let i = 1; i <= num; i++) {
-    result *= i;
+function findLongestWordLength(str) {
+  let longestWord = "";
+  let words = str.split(" ");
+  for (let i = 0; i < words.length; i += 1) {
+    let pickWord = words[i];
+    if (pickWord.length > longestWord.length) {
+      longestWord = pickWord;
+    }
   }
-  return result;
+
+  return longestWord.length;
 }
-factorialize(5);
+
+console.log(
+  findLongestWordLength("The quick brown fox jumped over the lazy dog")
+);
